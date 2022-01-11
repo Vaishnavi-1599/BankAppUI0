9e00837fc73698893322eadd5a38bcc619ca8194
Vaishnavi Kale <kalevaishnavi75@gmail.com>
2022-01-11 19:17:23 +0530

    spring form added

--- src/com/bankui/ServletConfig.java
+++ src/com/bankui/ServletConfig.java
@@ -1,16 +1,20 @@
 package com.bankui;
 
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.bankui.controller"})
-public class ServletConfig {
+@ComponentScan(basePackages = {"com.bankui.controller", "com.bankui.beans"})
+public class ServletConfig implements WebMvcConfigurer{
+
+
 	@Bean
 	public InternalResourceViewResolver viewResolver(){
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -18,6 +22,7 @@ public class ServletConfig {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
-	 
+	
+	
 	
 }
--- src/com/bankui/controller/AccountOpenController.java
+++ src/com/bankui/controller/AccountOpenController.java
@@ -1,12 +1,35 @@
 package com.bankui.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bankui.beans.Customer;
+import com.bankui.beans.UserInfo;
 
 @Controller
 public class AccountOpenController {
+	
+	
+	@Autowired
+	private Customer customer;
+ 
+	
+	
 	@RequestMapping("/")
-    public String showAccountOpenScreen() {
-	return "account_open";
+	public String showAccountOpenScreen (Model model) {
+		model.addAttribute("customer" , customer);
+	return "account_open";	
+	}	
+	
+	@RequestMapping("/create-account")
+	public String showLoginScreen(@ModelAttribute Customer customer) {
+		
+		
+		System.out.println(customer);
+		return "account_open";
+	
 }
-}
+	}
